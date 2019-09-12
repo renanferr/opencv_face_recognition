e91fc258e456e6e4c762c35001cea8e9ca7438f7
Renan Ferreira <renanferreira.js@gmail.com>
2019-09-12 15:14:43 -0300

    Add comments

--- face.py
+++ face.py
@@ -6,22 +6,31 @@ import argparse
 def detectAndDisplay(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
-    # -- Detect faces
+
     faces = face_cascade.detectMultiScale(frame_gray)
+
+    # DESENHAR CIRCULOS EM VOLTA DAS FACES ENCONTRADAS
+    #
     # for (x,y,w,h) in faces:
     #     center = (x + w//2, y + h//2)
-
+    #
     #     # frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
     #     faceROI = frame_gray[y:y+h,x:x+w]
-
-        #-- In each face, detect eyes
+    #
+        # DETECTAR E DESENHAR CIRCULOS NOS OLHOS DE CADA FACE
+        #
         # eyes = eyes_cascade.detectMultiScale(faceROI)
         # for (x2,y2,w2,h2) in eyes:
         #     eye_center = (x + x2 + w2//2, y + y2 + h2//2)
         #     radius = int(round((w2 + h2)*0.25))
         #     frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
     
-    # cv.imshow('Capture - Face detection', frame) # <- Não é necessário
+
+    # MOSTRAR JANELA COM IMAGEM DA CAMERA
+    #
+    # cv.imshow('Capture - Face detection', frame)
+    #
+    #
 
     if len(faces) > 0:
         retval, buf	= cv.imencode('.jpg', frame)
